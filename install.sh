3f50621b1c52d63c31ed89c4baf955cecac7de30
mitch092 <stevenm7861@gmail.com>
2025-01-05 05:09:44 -0800

    Inlined install2.sh and used arch-chroot.

--- install.sh
+++ install.sh
@@ -16,6 +16,8 @@ ROOT_LABEL="root"
 
 HOST_NAME="vengeance"
 ROOT_PASSWORD="changeme"
+FIRST_USER="steven"
+FIRST_USER_PASSWORD="changeme"
 
 # Partition Disk
 sgdisk --zap-all --clear \
@@ -57,11 +59,31 @@ sed -i '/%wheel ALL=(ALL:ALL) ALL/s/^# //g' "${MOUNT_DIR}/etc/sudoers"
 # Create a symbolic link for the systemd-resolved stub to resolv.conf.
 ln -sf "../run/systemd/resolve/stub-resolv.conf" "${MOUNT_DIR}/etc/resolv.conf"
 
-# Use systemd-nspawn to configure the rest of the system from inside a container, running another install script.
-systemd-nspawn --boot --notify-ready=yes --machine=installer --console=passive --bind=".:/scripts" --directory="${MOUNT_DIR}" &
-systemd-run --wait --machine=installer "/bin/bash /scripts/install2.sh; poweroff"
+# Using a heredoc to inline a script to run inside of arch-chroot to configure the rest.
+arch-chroot $MOUNT_DIR /bin/bash <<EOF
+set -e
 
-arch-chroot $MOUNT_DIR "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB; grub-mkconfig -o /boot/grub/grub.cfg"
+# Set time and date.
+timedatectl set-ntp true
+timedatectl set-local-rtc false
+
+# Set locale
+locale-gen
+
+# Add first non-root user.
+useradd -m -G wheel -s /bin/bash "${FIRST_USER}"
+echo "${FIRST_USER}:${FIRST_USER_PASSWORD}" | chpasswd
+
+pacman -S --noconfirm linux linux-firmware
+
+bootctl install
+
+# Enable various services.
+systemctl enable systemd-timesyncd.service
+systemctl enable systemd-networkd.service
+systemctl enable systemd-resolved.service
+systemctl enable systemd-boot-update.service
+EOF
 
 # Final Steps
 umount -R $MOUNT_DIR
