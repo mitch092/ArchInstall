fb6841d9396d5056e1ae9e792bcf85f7833fd435
mitch092 <stevenm7861@gmail.com>
2025-01-05 05:01:57 -0800

    Added systemd-firstboot.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ set -e
 # User set variables.
 DISK="/dev/sda"
 MOUNT_DIR="/mnt"
+BOOT_PATH="${MOUNT_DIR}/efi"
 
 EFI_SIZE="4GiB"
 SWAP_SIZE="8GiB"
@@ -13,7 +14,8 @@ EFI_LABEL="efi"
 SWAP_LABEL="swap"
 ROOT_LABEL="root"
 
-BOOT_PATH="${MOUNT_DIR}/efi"
+HOST_NAME="vengeance"
+ROOT_PASSWORD="changeme"
 
 # Partition Disk
 sgdisk --zap-all --clear \
@@ -36,6 +38,10 @@ swapon --discard -L $SWAP_LABEL
 # Install Base System
 pacstrap -K $MOUNT_DIR base sudo
 
+systemd-firstboot --root=$MOUNT_DIR --locale=en_US.UTF-8 --locale-messages=en_US.UTF-8 \
+    --keymap=us --timezone=America/Los_Angeles --hostname=$HOST_NAME --root-password=$ROOT_PASSWORD \
+    --root-shell=/bin/bash --kernel-command-line="root=LABEL=root rw" --setup-machine-id
+
 # Generate an fstab using labels.
 genfstab -L -p $MOUNT_DIR >"${MOUNT_DIR}/etc/fstab"
 
@@ -55,7 +61,7 @@ ln -sf "../run/systemd/resolve/stub-resolv.conf" "${MOUNT_DIR}/etc/resolv.conf"
 systemd-nspawn --boot --notify-ready=yes --machine=installer --console=passive --bind=".:/scripts" --directory="${MOUNT_DIR}" &
 systemd-run --wait --machine=installer "/bin/bash /scripts/install2.sh; poweroff"
 
-arch-chroot "${MOUNT_DIR}" "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB; grub-mkconfig -o /boot/grub/grub.cfg"
+arch-chroot $MOUNT_DIR "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB; grub-mkconfig -o /boot/grub/grub.cfg"
 
 # Final Steps
 umount -R $MOUNT_DIR
--- install2.sh
+++ install2.sh
@@ -3,12 +3,8 @@
 set -e
 
 # User set variables.
-HOST_NAME="vengeance"
-ROOT_PASSWORD="changeme"
 FIRST_USER="steven"
 FIRST_USER_PASSWORD="changeme"
-
-pacman -S --noconfirm linux linux-firmware
 
 # Set time and date.
 timedatectl set-ntp true
@@ -16,19 +12,17 @@ timedatectl set-local-rtc false
 
 # Set locale
 locale-gen
-localectl set-locale LANG=en_US.UTF-8
 
-# Set hostname.
-hostnamectl set-hostname "${HOST_NAME}"
-
-# Configure root password and add a non-root user.
-echo "root:${ROOT_PASSWORD}" | chpasswd
+# Add first non-root user.
 useradd -m -G wheel -s /bin/bash "${FIRST_USER}"
 echo "${FIRST_USER}:${FIRST_USER_PASSWORD}" | chpasswd
+
+pacman -S --noconfirm linux linux-firmware
 
 bootctl install
 
 # Enable various services.
+systemctl enable systemd-timesyncd.service
 systemctl enable systemd-networkd.service
 systemctl enable systemd-resolved.service
 systemctl enable systemd-boot-update.service
